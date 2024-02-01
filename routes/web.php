f148e1df5df9db81a80cb303a2c3056c2f50599b
vivien00000 <151801887+vivien00000@users.noreply.github.com>
2024-02-01 19:35:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
