efadad873025006f55958cbb2f53ad33f9b08238
vivien00000 <151801887+vivien00000@users.noreply.github.com>
2024-02-04 09:16:06 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route ::get('/user_login', function(){
+    return view('user_login');
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-Route::get('/', function () {
+Route ::get('/', function(){
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+
+Route ::get('/profile', function(){
+    return view('profile');
 });
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route ::get('/enroll', function(){
+    return view('enroll');
+});
 
-// Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route ::get('/user_page', function(){
+    return view('user_page');
+});
+
+Route ::get('/user_account', function(){
+    return view('user_account');
+});
+
+
